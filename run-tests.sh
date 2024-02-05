23d875ddc50e39297f558472a44f0d8b5f021a1b
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2024-02-05 12:48:33 -0600

    Print configuration on run

--- run-tests.sh
+++ run-tests.sh
@@ -5,6 +5,11 @@ skript_repo_ref="${INPUT_SKRIPT-REPO-REF}"
 run_vanilla_tests="${INPUT_RUN-VANILLA-TESTS}"
 skript_test_directory="/skript/src/test/skript/tests"
 
+echo "Configuration:"
+echo "  Test script directory: $test_script_directory"
+echo "  Skript repo ref: $skript_repo_ref"
+echo "  Run vanilla tests: $run_vanilla_tests"
+
 git clone --recurse-submodules https://github.com/SkriptLang/Skript.git /skript
 cd /skript || exit 1
 if [ -n "$skript_repo_ref" ]; then
