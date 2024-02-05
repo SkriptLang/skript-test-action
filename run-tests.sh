b609984456b64aef93552472d366ef9519e0d649
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2024-02-05 13:03:56 -0600

    Add vanilla test deletion log

--- run-tests.sh
+++ run-tests.sh
@@ -16,6 +16,7 @@ if [ -n "$skript_repo_ref" ]; then
   git checkout -f "$skript_repo_ref"
 fi
 if [ "$run_vanilla_tests" = "false" ]; then
+  echo "Deleting vanilla tests"
   rm -rf "${skript_test_directory:?}/*"
 fi
 cp -r "/github/workspace/$test_script_directory" "${skript_test_directory:?}/custom"
