d5eec84e7b048229414acf4d36fd815aaeca5ffd
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2024-02-05 12:00:52 -0600

    Add action

---
+++ skript-test-acton/run-tests.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -l
+
+test_script_directory="$1"
+skript_repo_ref="$2"
+run_vanilla_tests="$3"
+skript_test_directory="src/test/skript/tests"
+
+git clone --recurse-submodules https://github.com/SkriptLang/Skript.git
+cd Skript || exit 1
+if [ -n "$skript_repo_ref" ]; then
+  git checkout -f "$skript_repo_ref"
+fi
+if [ "$run_vanilla_tests" = "false" ]; then
+  rm -rf "${skript_test_directory:?}/*"
+fi
+cp "/github/workspace/$test_script_directory" "${skript_test_directory:?}/custom"
+./gradlew quickTest
