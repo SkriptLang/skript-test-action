d1a75d890b76a47f4b85d25f5bbb318b40d32e4c
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2024-02-05 12:42:02 -0600

    Hopefully make steps not conflict with eachother

--- run-tests.sh
+++ run-tests.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -l
 
-test_script_directory="$1"
-skript_repo_ref="$2"
-run_vanilla_tests="$3"
-skript_test_directory="src/test/skript/tests"
+test_script_directory="${INPUT_TEST-SCRIPT-DIRECTORY}"
+skript_repo_ref="${INPUT_SKRIPT-REPO-REF}"
+run_vanilla_tests="${INPUT_RUN-VANILLA-TESTS}"
+skript_test_directory="/skript/src/test/skript/tests"
 
-git clone --recurse-submodules https://github.com/SkriptLang/Skript.git
-cd Skript || exit 1
+git clone --recurse-submodules https://github.com/SkriptLang/Skript.git /skript
+cd /skript || exit 1
 if [ -n "$skript_repo_ref" ]; then
   git checkout -f "$skript_repo_ref"
 fi
