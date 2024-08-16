3dc25d3aa281dfd6a8cf1bd9ba9b7de385066de4
Pikachu920 <28607612+Pikachu920@users.noreply.github.com>
2024-08-15 22:51:03 -0500

    Change root directory of test runner

--- src/run-tests.py
+++ src/run-tests.py
@@ -19,18 +19,18 @@ class EnvironmentResource(TypedDict):
     target: str
 
 
-github_workspace_directory = Path("/github/workspace")
-test_script_directory = github_workspace_directory / os.environ['INPUT_TEST_SCRIPT_DIRECTORY']
+skript_repo_directory = Path("/skript")
+test_script_directory = skript_repo_directory / os.environ['INPUT_TEST_SCRIPT_DIRECTORY']
 skript_repo_ref = os.environ.get("INPUT_SKRIPT_REPO_REF", None)
 run_vanilla_tests = os.environ.get("INPUT_RUN_VANILLA_TESTS", None) == "true"
 skript_repo_git_url = "https://github.com/SkriptLang/Skript.git"
-skript_repo_path = github_workspace_directory / "skript"
+skript_repo_path = skript_repo_directory / "skript"
 skript_test_directory = skript_repo_path / "src" / "test" / "skript" / "tests"
 custom_test_directory = skript_test_directory / "custom"
 extra_plugins_directory = None
 extra_plugins_directory_string = os.environ.get("INPUT_EXTRA_PLUGINS_DIRECTORY", None)
 if extra_plugins_directory_string is not None and extra_plugins_directory_string != "":
-    extra_plugins_directory = github_workspace_directory / extra_plugins_directory_string
+    extra_plugins_directory = skript_repo_directory / extra_plugins_directory_string
 
 print("Configuration:")
 print(f"  Test script directory: {test_script_directory}")
